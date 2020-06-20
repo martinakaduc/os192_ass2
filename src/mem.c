05f8072bd01195ac1b1d362cd95af5efdbce3daf
Nguyen Quang Duc <nguyenquangduc2000@gmail.com>
2020-06-20 17:30:32 +0700

    Update code

--- src/mem.c
+++ src/mem.c
@@ -16,12 +16,14 @@ static struct
 } _mem_stat[NUM_PAGES];
 
 static pthread_mutex_t mem_lock;
+static pthread_mutex_t ram_lock;
 
 void init_mem(void)
 {
     memset(_mem_stat, 0, sizeof(*_mem_stat) * NUM_PAGES);
     memset(_ram, 0, sizeof(BYTE) * RAM_SIZE);
     pthread_mutex_init(&mem_lock, NULL);
+    pthread_mutex_init(&ram_lock, NULL);
 }
 
 /* get offset of the virtual address */
@@ -135,10 +137,12 @@ addr_t alloc_mem(uint32_t size, struct pcb_t *proc)
             cr_num_pages++;
         }
     }
-    if (cr_num_pages >= num_pages && proc->bp + num_pages * PAGE_SIZE < NUM_PAGES * PAGE_SIZE + PAGE_SIZE)
+
+    if (cr_num_pages >= num_pages && proc->bp + num_pages * PAGE_SIZE <= NUM_PAGES * PAGE_SIZE)
         mem_avail = 1;
     else
         ret_mem = 0;
+
     if (mem_avail)
     {
         /* We could allocate new memory region to the process */
@@ -159,12 +163,13 @@ addr_t alloc_mem(uint32_t size, struct pcb_t *proc)
                 _mem_stat[i].index = j;
                 if (j != 0)
                     _mem_stat[k].next = i;
-                /*------------------------*/
+
                 addr_t physical_addr = i << OFFSET_LEN;
                 addr_t first_lv = get_first_lv(ret_mem + j * PAGE_SIZE);
                 addr_t second_lv = get_second_lv(ret_mem + j * PAGE_SIZE);
                 // addr_t offset = get_offset(ret_mem + j * PAGE_SIZE);
                 int booler = 0;
+
                 /*Neu da co first index trong page table*/
                 for (int n = 0; n < proc->seg_table->size; n++)
                 {
@@ -190,8 +195,9 @@ addr_t alloc_mem(uint32_t size, struct pcb_t *proc)
                 }
                 /*------------------------*/
                 k = i;
-                j++;
-                num_pages_use++;
+                j += 1;
+                num_pages_use += 1;
+
                 if (num_pages_use == num_pages)
                 {
                     _mem_stat[k].next = -1;
@@ -200,21 +206,7 @@ addr_t alloc_mem(uint32_t size, struct pcb_t *proc)
             }
         }
     }
-    // printf("Cap phat cho process: %d %d trang\n",proc->pid,num_pages );
-    /*Code kiem tra bang phan trang va phan doan*/
-    // printf("Cap phat vung nho cho process:%d\n", proc->pid);
-    // for (int i = 0; i < proc->seg_table->size; i++)
-    // {
-    //     printf("seg table v index :%05x\n", proc->seg_table->table[i].v_index);
-    //     for (int j = 0; j < proc->seg_table->table[i].pages->size; j++)
-    //     {
-    //         printf("page table v index: %05x page: %05x\n", proc->seg_table->table[i].pages->table[j].v_index, proc->seg_table->table[i].pages->table[j].p_index);
-    //     }
-    // }
-    // dump();
-    /*Kiem tra _mem_stat*/
-    // printf("alloccccccccccc\n");
-    // dump();
+
     pthread_mutex_unlock(&mem_lock);
     return ret_mem;
 }
@@ -234,7 +226,6 @@ int free_mem(addr_t address, struct pcb_t *proc)
     addr_t physical_addr;
     if (translate(address, &physical_addr, proc))
     {
-        // int number1=0,first_num=0;
         int next = -2;
         int i = 0, j = 0;
         /*Xoa tim ra vi tri ung voi address*/
@@ -242,7 +233,6 @@ int free_mem(addr_t address, struct pcb_t *proc)
         {
             if (physical_addr == i << OFFSET_LEN)
             {
-                // first_num=i;
                 break;
             }
         }
@@ -253,7 +243,7 @@ int free_mem(addr_t address, struct pcb_t *proc)
             // number1++;
             _mem_stat[next].proc = 0;
             next = _mem_stat[next].next;
-            /*----------------*/
+
             /*Xoa seg table va page table*/
             addr_t first_lv = get_first_lv(address + j * PAGE_SIZE);
             addr_t second_lv = get_second_lv(address + j * PAGE_SIZE);
@@ -265,7 +255,6 @@ int free_mem(addr_t address, struct pcb_t *proc)
                     {
                         if (proc->seg_table->table[n].pages->table[m].v_index == second_lv)
                         {
-                            /*----------------*/
                             /*Don page table lai, xoa phan tu cuoi cung*/
                             int k = 0;
                             for (k = m; k < proc->seg_table->table[n].pages->size - 1; k++)
@@ -273,10 +262,11 @@ int free_mem(addr_t address, struct pcb_t *proc)
                                 proc->seg_table->table[n].pages->table[k].v_index = proc->seg_table->table[n].pages->table[k + 1].v_index;
                                 proc->seg_table->table[n].pages->table[k].p_index = proc->seg_table->table[n].pages->table[k + 1].p_index;
                             }
+
                             proc->seg_table->table[n].pages->table[k].v_index = 0;
                             proc->seg_table->table[n].pages->table[k].p_index = 0;
                             proc->seg_table->table[n].pages->size--;
-                            /*----------------*/
+
                             break;
                         }
                     }
@@ -285,11 +275,13 @@ int free_mem(addr_t address, struct pcb_t *proc)
                         /* Neu page-table do rong thi xoa di */
                         free(proc->seg_table->table[n].pages);
                         int m = 0;
+
                         for (m = n; m < proc->seg_table->size - 1; m++)
                         {
                             proc->seg_table->table[m].v_index = proc->seg_table->table[m + 1].v_index;
                             proc->seg_table->table[m].pages = proc->seg_table->table[m + 1].pages;
                         }
+
                         proc->seg_table->table[m].v_index = 0;
                         proc->seg_table->table[m].pages = NULL;
                         proc->seg_table->size--;
@@ -299,23 +291,8 @@ int free_mem(addr_t address, struct pcb_t *proc)
             }
             j++;
         }
-        // printf("Giai phong %d trang nho bat dau tu trang %d\n",number1,first_num );
     }
 
-    /*Code kiem tra bang phan trang va phan doan*/
-    // printf("Giai phong vung nho cho process:%d\n", proc->pid);
-    // for (int i = 0; i < proc->seg_table->size; i++)
-    // {
-    //     printf("seg table v index :%05x\n", proc->seg_table->table[i].v_index);
-    //     for (int j = 0; j < proc->seg_table->table[i].pages->size; j++)
-    //     {
-    //         printf("page table v index: %05x page: %05x\n", proc->seg_table->table[i].pages->table[j].v_index, proc->seg_table->table[i].pages->table[j].p_index);
-    //     }
-    // }
-    // dump();
-    /*Code kiem tra _mem_stat*/
-    // printf("freeeeeeeeeee\n");
-    // dump();
     pthread_mutex_unlock(&mem_lock);
     return 0;
 }
@@ -336,11 +313,14 @@ int read_mem(addr_t address, struct pcb_t *proc, BYTE *data)
 
 int write_mem(addr_t address, struct pcb_t *proc, BYTE data)
 {
+
     addr_t physical_addr;
     if (translate(address, &physical_addr, proc))
     {
+        pthread_mutex_unlock(&ram_lock);
         _ram[physical_addr] = data;
-        // dump();
+        pthread_mutex_unlock(&ram_lock);
+
         return 0;
     }
     else
